eeaeb03c486765d9b2d89e8952eb0e13836daae8
Richard <richardwangwang@gmail.com>
2016-05-09 22:40:03 +0800

    Remove dragtable.

--- django_auto_filter/static/js/django_auto_filter.js
+++ django_auto_filter/static/js/django_auto_filter.js
@@ -1,6 +1,9 @@
 $(document).ready( function () {
     $('table').bootstrapTable();
-    $('table').dragtable();
+//    $('table').dragtable({
+//        dragHandle:'.th-inner'
+////        dragaccept:'th-inner'
+//    });
 //    $('table').DataTable( {
 ////        dom: 'Bfrtip',
 //        dom: 'BHt',
