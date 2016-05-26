139017ea30f30cc994e208a6df25f0503de30d36
Richard <richardwangwang@gmail.com>
2016-05-26 23:00:03 +0800

    Added ajax field.

--- django_auto_filter/views_django_auto_filter.py
+++ django_auto_filter/views_django_auto_filter.py
@@ -1,3 +1,4 @@
+from ajax_select import make_ajax_form
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
@@ -11,6 +12,8 @@ from django_tables2_reports.utils import create_report_http_response
 import django_tables2 as tables
 from django_tables2.utils import A  # alias for Accessor
 
+from djangoautoconf.model_utils.model_attr_utils import enum_model_fields
+
 __author__ = 'weijia'
 
 
@@ -18,7 +21,12 @@ class DjangoAutoFilter(TemplateView):
     template_name = 'django_auto_filter/filters.html'
     model_class = User
     filter_fields = {"username": ["icontains"]}
+    # The following is also OK
+    filter_fields = ["username", ]
     edit_namespace = "admin"
+    # Used by django-ajax-selects
+    ajax_fields = {}
+    # ajax_fields = {"relations": "ufs_obj", "parent": "ufs_obj", "descriptions": "description"}
 
     def __init__(self, **kwargs):
         super(DjangoAutoFilter, self).__init__(**kwargs)
@@ -48,11 +56,24 @@ class DjangoAutoFilter(TemplateView):
 
     def get_filter_class(self):
 
+        ajax_form = make_ajax_form(self.model_class,
+                                   self.ajax_fields,
+                                   self.get_contains_all_keyword_form())
+
+        for field in ajax_form.declared_fields:
+            ajax_form.declared_fields[field].required = False
+
+        remaining_fields = []
+
+        for attr in enum_model_fields(self.model_class):
+            if attr.name not in self.ajax_fields:
+                remaining_fields.append(attr.name)
+
         filter_class = type(self.model_class.__name__ + "AutoFilter", (django_filters.FilterSet,), {
             "Meta": type("Meta", (),
                          {"model": self.model_class,
-                          "form": self.get_contains_all_keyword_form(),
-                          # "fields": self.filter_fields
+                          "form": ajax_form,
+                          "fields": remaining_fields,
                           })
         })
         return filter_class
@@ -87,5 +108,5 @@ class DjangoAutoFilter(TemplateView):
         content_type = ContentType.objects.get_for_model(self.model_class)
         url_name = "%s:%s_%s_change" % (self.edit_namespace, content_type.app_label, content_type.model)
         url = urlresolvers.reverse(url_name, args=("1",))
-        url = url.replace("1",  "%d")
+        url = url.replace("1", "%d")
         return url
