05a0e054ded85657a8f7d21ec0a3abe42a24f00c
Richard <richardwangwang@gmail.com>
2016-11-20 13:14:44 +0800

    Fix import issue.

--- django_auto_filter/filter_for_models.py
+++ django_auto_filter/filter_for_models.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from django_auto_filter.views_django_auto_filter_new import DjangoAutoFilter
+from django_auto_filter.views_django_auto_filter import DjangoAutoFilter
 from djangoautoconf.model_utils.model_attr_utils import model_enumerator
 from ufs_tools.string_tools import class_name_to_low_case
 
